bbcacce34539fd3b0e8c9b8621d1e453b2e1cacd
mbarrenecheajr <mbarrenecheajr@gmail.com>
2012-04-08 22:21:53 -0600

    forget to include main.py

---
+++ cuboulder/csci5454/hw4/src/main.py
@@ -0,0 +1,20 @@
+'''
+Created on Apr 8, 2012
+
+@author: Mario Barrenechea
+'''
+
+import ising
+Ising = ising.Ising
+
+n = 10
+temp = 1000
+alpha = 1-10e-4
+
+i = Ising(n,temp,alpha)
+
+print i
+
+#now, run simulated_annealing()
+i.simulate_annealing()
+
